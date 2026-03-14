0889d7cf160165284439aaa2f160d74cf3f78faa
poorni141 <poornithaarumugam@gmail.com>
2026-03-14 22:00:37 +0530

    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Smooth Scroll
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href'))
+            .scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+// Contact Form Message
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thank you for contacting me! 😊");
+    this.reset();
+});
